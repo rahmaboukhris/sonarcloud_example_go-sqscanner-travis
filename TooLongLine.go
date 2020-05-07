dbb8c65ef800c66658fdbe81525eaea3b1d8a23f
rahmaboukhris <64844430+rahmaboukhris@users.noreply.github.com>
2020-05-07 22:11:09 +0200

    Update TooLongLine.go

--- TooLongLine.go
+++ TooLongLine.go
@@ -2,6 +2,7 @@
 package sonarcloud_go_qscanner_travis
 
 func longLine() int {
+rrr
   x := 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
   // Noncompliant@+1 {{Split this 122 characters long line (which is greater than 120 authorized).}}
   return x + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
